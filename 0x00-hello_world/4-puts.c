da1610da568f50c677f24cc6cd6d019fe740eb91
dadadupeola <dadadupeolagmail.com>
2022-10-17 19:03:00 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - print a line of code using puts
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like buildig a multilingual puzzle");
+	return (0);
+}
