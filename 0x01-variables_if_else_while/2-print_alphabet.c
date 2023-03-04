e8f19556d40f35b6920f42b83ff2f5683d54d5d0
dadadupeola <dadadupeola@gmail.com>
2023-03-04 05:12:44 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	char C;
+
+	for (C = 'a'; C <= 'z'; C++)
+	putchar(C);
+	putchar('\n');
+	return (0);
+
+}
