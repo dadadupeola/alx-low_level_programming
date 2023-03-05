6b6467e5c7931907c6cd2a99ccc650aa079f7515
dadadupeola <dadadupeola@gmail.com>
2023-03-04 23:18:41 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
 #include "main.h"
+
 /**
  * main - Entry point
  * Description: 'prints _putchar to stdout'
- * Return: always O
+ * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,4 +18,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _islower - Shows 1 if the nput is a
- * lowercase character. Another cases, shows
+ * _islower - Shows 1 if the input is a
+ * lowercase character. Another case, shows
  * 0
  *
  * @c: The character in ASCII code
