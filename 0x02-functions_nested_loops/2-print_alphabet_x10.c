e05612a6ac9c83bc5f6cfd387fe13755b713b161
dadadupeola <dadadupeola@gmail.com>
2023-03-04 23:47:40 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,22 @@
 #include "main.h"
-/**
- * print_alphabet_x10 - Prints the alphabet 10 times.
- *
- * Return: Always 0 (Success)
- */
-void print_alphabet_x10(void)
-{
-	int i;
-	int j;
 
-	for (j = 1; i <= 10; i++)
+/**
+ * print_alphabet_x10 - Prints the alphabet x10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i = 0;
+
+	while (i <= 9)
 	{
-		for (j = 97; j <= 122; j++)
-		{
-		_putchar(j);
-		}
-		_putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 	}
 }
-	  
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <unistd.h>
+
 /**
  * _putchar - this writes the character c to stdout
  * @c: This is the character to print
- *
  * Return: On success 1.
  * On error, -1 will be returned, and the error is set accordingly.
  */
